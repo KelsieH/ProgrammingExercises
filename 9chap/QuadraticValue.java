353b8e8788ace7fb8c59ed8a420a8d51ff5b5537
Kelsie He <hekelsie@gmail.com>
2020-09-10 11:02:12 -0600

    exercise 2

---
+++ 9chap/QuadraticValue.java
@@ -0,0 +1,10 @@
+public class QuadraticValue
+{
+    public static void main ( String[] args )
+    {
+        double x = 5;
+        double y = ((3 * (x*x)) - (8 * x) + (4));
+
+        System.out.println("At X = " + x + ", the value is " + y);
+    }
+}
