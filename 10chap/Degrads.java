33a477fb77138c358cf0b869159a5fafbea26e80
Kelsie He <hekelsie@gmail.com>
2020-09-14 22:06:10 -0600

    make a few changes to exercises

---
+++ 10chap/Degrads.java
@@ -0,0 +1,10 @@
+public class Degrads
+{
+    public static void main ( String[] args )
+    {
+        double degrees = 30;
+        double rad = (degrees * (Math.PI/180));
+
+        System.out.println(degrees + " degrees is equal to: " + rad + " radians");
+    }
+}
---
+++ 10chap/Rainfall.java
@@ -0,0 +1,11 @@
+public class Rainfall
+{
+    public static void main ( String[] args )
+    {
+        int april = 12;
+        int may = 14;
+        int june = 8;
+        double average = (april + may + june)/3;
+        System.out.println("Rainfall for April: " + april + "\t Rainfall for May: " + may + "\t Rainfall for June: " + june + "\t Average Rainfall: " + average);
+    }
+}
