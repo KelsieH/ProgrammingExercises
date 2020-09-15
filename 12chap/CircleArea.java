6255f030534cdb42d96e30061ddd3aafbdc6edc5
Kelsie He <hekelsie@gmail.com>
2020-09-14 22:44:28 -0600

    exercise one

---
+++ 12chap/CircleArea.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CircleArea
+{
+    public static void main (String[] args)
+    {
+        int radius;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Input the radius: ");
+        radius = scan.nextInt();
+
+        double area;
+        area = ((Math.PI) * (radius * radius));
+
+        System.out.println("The radius is: " + radius + "The area is: " + area);
+    }
+}
