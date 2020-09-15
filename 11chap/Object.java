9412a972be370ccb0281ea45f5d50a008c44739c
Kelsie He <hekelsie@gmail.com>
2020-09-14 22:07:29 -0600

    exercises 1-3

---
+++ 11chap/Object.java
@@ -0,0 +1,11 @@
+public class Object 
+{
+    public static void main ( String[] args )
+    { String str ;
+        str = new String(
+            "Hello World"
+        );
+    
+        System.out.println( str );
+    }
+}
