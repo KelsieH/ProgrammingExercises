e512b6ac4218135e1a01f735b1b5efcfebe298f1
Kelsie He <hekelsie@gmail.com>
2020-09-14 23:15:59 -0600

    exercise 4

---
+++ 12chap/Ohm.java
@@ -0,0 +1,11 @@
+public class Ohm
+{
+    public static void main(String[] args)
+    {
+        double V = 5;
+        double R = 2;
+        double I = ((V + 0.0)/R);
+
+        System.out.println( I );
+    }
+}
